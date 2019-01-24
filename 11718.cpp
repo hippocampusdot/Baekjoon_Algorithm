c7b6b221266ce1a4bbf4167e3828864de583f283
hippocampusdot <hippocampusdot@gmail.com>
2019-01-24 23:43:17 +0900

    add

--- 11718.cpp
+++ 11718.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main(void) {
+	int MAX = 100;
 	string s;
-	
-	cin>>s;
-	cout<<s;
-
+	while(MAX > 0) {
+		getline(cin,s);
+		if(s=="")
+			break;
+		cout<<s<<endl;
+		MAX--;
+	}
 	return 0;
 }
