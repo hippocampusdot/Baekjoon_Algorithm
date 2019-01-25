bfe3cd05e90fa0fd20a8fa331c2b6b3526b1d71d
hippocampusdot <hippocampusdot@gmail.com>
2019-01-25 16:56:11 +0900

    add

---
+++ 2441.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	int N;
+
+	cin>>N;
+
+	for(int i=N; i>=0; i--){
+		for(int b = N-i; b>0; b--)
+			cout<<" ";
+		for(int j=1; j<=i; j++)
+			cout<<"*";
+		cout<<endl;
+	}
+	return 0;
+}
